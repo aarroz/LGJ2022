c895b6a0086c1d572831513f8727fa989dcdddb8
Aaron <aarroz@users.noreply.github.com>
2022-04-26 14:37:44 -0700

    Added dialogue and triggers

--- scenes/AudioManager.gd
+++ scenes/AudioManager.gd
@@ -1,21 +1,87 @@
 extends Spatial
 
+const Line01_Intro = preload("res://sounds/dial/Line01_Intro.ogg")
+const Line02_First = preload("res://sounds/dial/Line02_FirstPuzzle.ogg")
+const Line03_Wire = preload("res://sounds/dial/Line03_WireSwitch.ogg")
+const Line04_iMiss = preload("res://sounds/dial/Line04_iMissThem.ogg")
+const Line05_Secu = preload("res://sounds/dial/Line05_SecurityBox.ogg")
+const Line06_Emplo = preload("res://sounds/dial/Line06_EmployeeSecrets.ogg")
+const Line07_Bril = preload("res://sounds/dial/Line07_BrilliantBear.ogg")
+const Line08_Seas = preload("res://sounds/dial/Line08_SeasonedEmployee.ogg")
+const Line09_Final = preload("res://sounds/dial/Line09_Finale.ogg")
+const Line10_Escape = preload("res://sounds/dial/Line10_Escape.ogg")
+
+var line01_finished = false
+var line02_finished = false
+var line03_finished = false
+var line04_finished = false
+var line05_finished = false
+var line06_finished = false
+var line07_finished = false
+var line08_finished = false
+var line09_finished = false
+var line10_finished = false
 
 func _ready():
 	$Faders.play("fade_out")
+	$Dial.set_stream(Line01_Intro)
+	$Dial.play()
 
 
 func _on_Trigger_01_Complete_body_entered(body):
-	if body.is_in_group("dynamic") and $Music.playing == false:
+	if body.is_in_group("dynamic") and not line03_finished and $Music.playing == false:
+		$Dial.set_stream(Line03_Wire)
+		$Dial.play()
+		yield(get_tree().create_timer(4.0), "timeout")
 		$Music.play()
 		$Faders.play("fade_in")
+		
+
+
+func _on_Trigger_04_02_Dialogue_body_entered(body):
+	if body.is_in_group("dynamic") and not line06_finished:
+		$Dial.set_stream(Line06_Emplo)
+		$Dial.play()
+		yield(get_tree().create_timer(16.0), "timeout")
+		$Music.play()
+		$Faders.play("fade_in")
+		line06_finished = true
+
+func _on_Trigger_FINALE_body_entered(body):
+	if body.is_in_group("dynamic") and not line09_finished:
+		$Dial.set_stream(Line09_Final)
+		$Dial.play()
+		line09_finished = true
+
+
+func _on_Trigger_00_Complete_body_entered(body):
+	if body.is_in_group("dynamic") and not line01_finished:
+		$Dial.set_stream(Line02_First)
+		$Dial.play()
+		line01_finished = true
+
+
+func _on_Trigger_04_Complete_body_entered(body):
+	if body.is_in_group("dynamic") and not line05_finished:
+		$Dial.set_stream(Line05_Secu)
+		$Dial.play()
+		line05_finished = true
+		$Faders.play("fade_out")
+
+
+func _on_Trigger_02_Complete_body_entered(body):
+	pass
+
+
+func _on_Trigger_03_Complete_body_entered(body):
+	if body.is_in_group("dynamic") and not line04_finished:
+		$Dial.set_stream(Line04_iMiss)
+		$Dial.play()
+		line04_finished = true
 
 
 func _on_Trigger_05_Complete_body_entered(body):
-	# Put a line here / First Box Maze Puzzle
-	pass # Replace with function body.
-
-
-func _on_Trigger_FINALE_body_entered(body):
-	# Put final dialogue and end game here
-	pass # Replace with function body.
+	if body.is_in_group("dynamic") and not line06_finished:
+		$Dial.set_stream(Line06_Emplo)
+		$Dial.play()
+		line06_finished = true
--- scenes/System_Door.gd
+++ scenes/System_Door.gd
@@ -1,24 +1,37 @@
 extends Spatial
 
 var opened = false
+onready var door_indic = $Door_00_Indicator
+onready var sound = $Sound_Door
+
+var dial_complete = false
+
+func _ready():
+	door_indic.material.set_albedo(Color(255, 0, 0, 0))
+	yield(get_tree().create_timer(23.0), "timeout")
+	print("end")
+	door_indic.material.set_albedo(Color(0, 255, 0, 0))
+	dial_complete = true
 
 func open():
 	$AnimationPlayer.play("open")
+	sound.play()
 	opened = true
 	
 func close():
 	$AnimationPlayer.play_backwards("open")
+	sound.play()
 	opened = false
 
 func _on_Button_body_entered(body):
-	if body.is_in_group("dynamic") and not opened:
+	if body.is_in_group("dynamic") and dial_complete and not opened:
 		open()
 	else:
 		pass
 
 
 func _on_Trigger_Close_body_entered(body):
-	if body.is_in_group("dynamic") and opened:
+	if body.is_in_group("dynamic") and dial_complete and opened:
 		close()
 	else:
 		pass
