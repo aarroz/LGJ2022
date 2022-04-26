8327803d52733214d668b5561ba77f415559e444
Aaron <aarroz@users.noreply.github.com>
2022-04-25 22:19:20 -0700

    Added triggers for later

--- scenes/AudioManager.gd
+++ scenes/AudioManager.gd
@@ -9,3 +9,13 @@ func _on_Trigger_01_Complete_body_entered(body):
 	if body.is_in_group("dynamic") and $Music.playing == false:
 		$Music.play()
 		$Faders.play("fade_in")
+
+
+func _on_Trigger_05_Complete_body_entered(body):
+	# Put a line here / First Box Maze Puzzle
+	pass # Replace with function body.
+
+
+func _on_Trigger_FINALE_body_entered(body):
+	# Put final dialogue and end game here
+	pass # Replace with function body.
