62223879778b4a407a5821d79ec17cbaf31670e0
aarroz <aarroz@users.noreply.github.com>
2022-05-12 22:04:24 -0700

    Changed scene loader

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,8 +3,6 @@ extends Control
 onready var settings_menu = $Settings
 onready var transition = $CanvasLayer/AnimationPlayer
 
-const game = preload("res://scenes/Level_00.tscn")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -22,7 +20,7 @@ func _on_Play_pressed():
 	transition.play("cam_fade_in")
 	$music.play("music_fade_out")
 	yield(get_tree().create_timer(2.0), "timeout")
-	get_tree().change_scene("res://scenes/Level_00.tscn")
+	Global.goto_scene("res://scenes/Level_00.tscn")
 
 
 func _on_Quit_pressed():
